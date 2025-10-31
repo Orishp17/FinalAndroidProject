42893862e9d5b907fcfd6e105e57de1ae38e40b8
Orishp17 <orishp17@gmail.com>
2025-10-31 10:38:54 +0200

    Update _com_example_androidproject_ui_main_posts_NpmRegistryFragment_GeneratedInjector.java

--- app/build/generated/source/kapt/debug/hilt_aggregated_deps/_com_example_androidproject_ui_main_posts_NpmRegistryFragment_GeneratedInjector.java
+++ app/build/generated/source/kapt/debug/hilt_aggregated_deps/_com_example_androidproject_ui_main_posts_NpmRegistryFragment_GeneratedInjector.java
@@ -2,6 +2,7 @@ package hilt_aggregated_deps;
 
 import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
 
+
 /**
  * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
  */
