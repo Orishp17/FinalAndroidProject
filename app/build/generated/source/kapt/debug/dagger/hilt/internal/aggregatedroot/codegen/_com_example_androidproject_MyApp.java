3a626796adec4454507289281f03951eb464f970
Orishp17 <orishp17@gmail.com>
2025-10-31 10:37:49 +0200

    Update _com_example_androidproject_MyApp.java

--- app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_androidproject_MyApp.java
+++ app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_androidproject_MyApp.java
@@ -3,6 +3,7 @@ package dagger.hilt.internal.aggregatedroot.codegen;
 import dagger.hilt.android.HiltAndroidApp;
 import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
 
+
 /**
  * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
  */
